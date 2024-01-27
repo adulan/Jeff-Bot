f43c90c7cbbffae73330abd1408d9d1fcd2413a0
Alec <alec.dulan@gmail.com>
2024-01-27 11:57:22 -0700

    Add Docker stuff

--- setup.py
+++ setup.py
@@ -1,42 +1,21 @@
-"""
-MIT License
+import os
 
-Copyright (c) 2018-2019 Evan Liapakis
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-"""
-
+def init_files():
 # Create all needed files
-with open("players.csv", 'w+'):
-    pass
+    with open("players.csv", 'w+'):
+        pass
 
-with open("tribes.csv", 'w+') as f:
-    f.write("voting,none\n")
+    with open("tribes.csv", 'w+') as f:
+        f.write("voting,none\n")
 
-with open("vote_time", 'w+') as f:
-    f.write('0')
+    with open("vote_time", 'w+') as f:
+        f.write('0')
 
-with open("idols.csv", 'w+') as f:
-    pass
+    with open("idols.csv", 'w+') as f:
+        pass
 
-with open("token", 'w+') as f:
-    f.write(input("Token: "))
+    with open("token", 'w+') as f:
+        f.write(os.getenv("DISCORD_TOKEN"))
 
-with open("playernum", 'w+') as f:
-    f.write("0")
+    with open("playernum", 'w+') as f:
+        f.write("0")
